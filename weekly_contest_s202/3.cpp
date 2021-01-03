e5acb7d4673821b5cc20aedebaaced2998eb5daa
old-yan <oldyan_zh@163.com>
2021-01-03 12:23:49 +0800

    contest_s202

--- weekly_contest_s202/3.cpp
+++ weekly_contest_s202/3.cpp
@@ -3,9 +3,27 @@
 class Solution {
 public:
     int waysToSplit(vector<int>& nums) {
-        //求出前缀和数组
+        //求出前缀和数组，sum.size()==nums.size()+1
         auto sum=presum(nums);
         int total=sum.back();
+        //三个指针(按照从1开始到n，而非从0开始到n-1)
+        //第一个指针i是左区间的尾坐标
+        //第二个指针j是中区间的尾坐标，最左能取哪里
+        //第三个指针k是右区间的头坐标，最右能取哪里
+        int ans=0;
+        for(int i=1,j=1,k=1;i<=nums.size()-2&&sum[i]<=total/3;i++){
+            int left_sum=sum[i];
+            //middle_sum必须处于left_sum*2到left_sum+(total-left_sum)/2之间
+            while(j==i||(sum[j]<left_sum*2)){
+                j++;
+            }
+            while(k<nums.size()&&sum[k]<=left_sum+(total-left_sum)/2){
+                k++;
+            }
+            //中区间的尾巴可以取j~k-1之间的值
+            ans=(ans+k-j)%MOD;
+        }
+        return ans;
     }
 };
 
---
+++ weekly_contest_s202/4.cpp
@@ -0,0 +1,42 @@
+#include "utils.h"
+
+class Solution {
+public:
+    //本题可以理解为求target和arr中出现的最长公共子序列长度
+    int minOperations(vector<int>& target, vector<int>& arr) {
+        //线段树是统计一个区间之内的最大值的有力工具
+        zkwMaxTree T(target.size());
+        //M记录每个数字对应的在target数组中的位置(target中元素不重复)
+        unordered_map<int,int>M;
+        for(int i=0;i<target.size();i++)M[target[i]]=i;
+        int k=0;
+        for(int i=0;i<arr.size();i++){
+            //对arr中每个元素，统计以其为结尾的最长公共子序列长度
+            int j=arr[i];
+            if(M.count(j)){
+                //T[i]表示target[i]及之前出现的最长公共子序列
+                //比如当前元素j在target中下标为M[j]=5
+                //利用线段树统计[0,4]间的最大值，即以下标0~4为结尾的最长公共子序列长度
+                k=T(0,M[j]-1);
+                //再加一，就是以下标5为结尾的最长公共子序列长度，记录到树中
+                T.set(M[j],k+1);
+            }
+        }
+        //统计出最长公共子序列，再把缺的补上即可
+        return target.size()-T(0,target.size()-1);
+    }
+};
+
+int main()
+{
+    cout<<boolalpha;
+    Solution sol;
+
+    vi target{6,4,8,1,3,2};
+    vi arr{4,7,6,2,3,8,6,1};
+    auto ans=sol.minOperations(target,arr);
+    DBG(ans);
+
+    system("pause");
+    return 0;
+}
