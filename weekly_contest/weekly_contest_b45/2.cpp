72786b6e57defe2619e32b8c26c8a7974997b8c2
old-yan <oldyan_zh@163.com>
2021-02-07 08:08:59 +0800

    contest_b45

--- weekly_contest/weekly_contest_b45/2.cpp
+++ weekly_contest/weekly_contest_b45/2.cpp
@@ -6,21 +6,9 @@ public:
     int maxAbsoluteSum(vector<int>& nums) {
         int ans=0;
         //sum记录前缀和
-        vi sum{0};
-        for(int a:nums)sum.pb(sum.back()+a);
-        //afterMax记录sum数组某个位置之后的最大值
-        int afterMax[sum.size()];
-        REPR(i,sum.size()-1)afterMax[i]=i==sum.size()-1?sum[i]:max(sum[i],afterMax[i+1]);
-        //afterMin记录sum数组某个位置之后的最小值
-        int afterMin[sum.size()];
-        REPR(i,sum.size()-1)afterMin[i]=i==sum.size()-1?sum[i]:min(sum[i],afterMin[i+1]);
-        REP(i,nums.size()){
-            //可能会正的很大
-            chmax(ans,abs(afterMax[i]-sum[i]));
-            //可能会负的很小
-            chmax(ans,abs(afterMin[i]-sum[i]));
-        }
-        return ans;
+        auto sum=presum(nums);
+        //答案就是前缀和数组中，最高峰与最低谷的差
+        return *max_element(ALL(sum))-*min_element(ALL(sum));
     }
 };
 
